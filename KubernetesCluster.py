a919a8be7a992cacb0028a08a140a6219889d867
surfingdoggo <c@surfingdoggo.com>
2024-06-02 09:40:13 -0500

    KubernetesCluster.py init class EKSCluster()

---
+++ KubernetesCluster.py
@@ -0,0 +1,14 @@
+class EKSCluster:
+    """
+    hi
+    """
+    def __init__(
+        self,
+        stack_info: dict,
+    ):
+        """
+        hi
+        """
+        self.stack_info: dict = stack_info
+
+        return
